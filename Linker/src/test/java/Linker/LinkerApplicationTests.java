e376dd9dc3358d496afec9b735ad6b6906515a83
lyj <kasean@nate.com>
2023-10-21 18:25:52 +0900

    first commit

---
+++ Linker/src/test/java/Linker/LinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package Linker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
