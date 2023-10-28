7841692ef4f0148c2009a53c56f0145233492fba
Jihye Kwon <ggung0929@gmail.com>
2023-10-28 11:38:48 +0900

    10.28 용어정리, 주석추가

--- Linker/src/main/java/Linker/repository/LabRepository.java
+++ Linker/src/main/java/Linker/repository/LabRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import Linker.model.Lab;
 
 public interface LabRepository extends JpaRepository<Lab, Integer> {
-	// 학원id리스트로 학원정보 가져오기
+	// 학원id 리스트로 검색해서 학원정보 리스트로 가져오기
 	List<Lab> findByLabIdIn(List<Integer> labIds);
 }
